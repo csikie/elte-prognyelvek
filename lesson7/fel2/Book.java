449046252e7d182c30f9c65dffe21de208371782
csikie <cs.erik18@gmail.com>
2021-11-04 12:03:29 +0100

    lesson 7

--- lesson7/fel2/Book.java
+++ lesson7/fel2/Book.java
@@ -1,16 +1,20 @@
+
+
 public class Book {
     static enum Genre {
         FANTASY, SATIRE, SCIFI, PHILOSOPHY, EDUCATIONAL;
     }
 
-    private String author;
-    private String title;
-    private int reservePrice;
-    private int id;
-    private Genre genre;
+    private final String author;
+    private final String title;
+    private final int reservePrice;
+    private final int id;
+    private final Genre genre;
 
-    private static int lastId;
+    private static int lastId; 
     public static void resetId() { lastId = 0; }
+
+    public int getReservePrice() { return reservePrice; }
     public int getId() { return id; }
 
     private Book(String author, String title, Genre genre, int reservePrice) {
@@ -18,37 +22,42 @@ public class Book {
         this.title = title;
         this.reservePrice = reservePrice;
         this.genre = genre;
-
         id = lastId++;
     }
 
-    public static Book make(String author, String title, String genreName, String reservePriceStr) {
+    public static Book make(String artist, String title, String genreName, String reservePriceStr) {      
         try {
             int reservePrice = Integer.parseInt(reservePriceStr);
 
-            if (author == null || title == null || title.length() < 2 || reservePrice <= 0) {
+            if (artist == null || title == null || title.length() < 2 || reservePrice <= 0)
                 return null;
-            }
 
-            Genre genre = Genre.valueOf(genreName);
+            Genre g = Genre.valueOf(genreName);
 
             int i = 0;
             while (i < title.length()) {
-                char c = title.charAt(i);
-                if (Character.isLetter(c) || Character.isDigit(c) || Character.isWhitespace(c)) {
-                    i++;
-                }
-                else {
+                if (Character.isLetter(title.charAt(i)) || Character.isDigit(title.charAt(i)) || Character.isWhitespace(title.charAt(i))) {
+                    ++i;
+                } else {
                     return null;
                 }
             }
-            return new Book(author, title, genre, reservePrice);
-        }
-        catch (NumberFormatException e) {
+            return new Book(artist, title, g, reservePrice);
+        } catch (NumberFormatException e) {
+            return null;
+        } catch (IllegalArgumentException e) {
             return null;
         }
-        catch (IllegalArgumentException e) {
-            return null;
+    }
+
+    public static boolean isSameGenre(Book b1, Book b2) {
+        return b1.genre == b2.genre;
+    }
+
+    public int compare(Book that) {
+        if (!isSameGenre(this, that)) {
+            throw new IllegalArgumentException();
         }
+        return Integer.compare(reservePrice, that.reservePrice);
     }
 }
